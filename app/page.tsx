94fa572d840372772ec3c09f80018912f29e6494
nayanpai <nayanspai@gmail.com>
2025-01-23 19:39:15 -0600

    landing screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">
-        SkyDrive: The last storage solution you'll need.
-      </h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">SkyDrive: The last storage solution you'll need.</h1>
     </div>
   );
 }
