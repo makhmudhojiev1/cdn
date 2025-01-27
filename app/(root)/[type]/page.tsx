5bd2b1bf266ab0488148b79f9d9d58e09dea4634
nayanpai <nayanspai@gmail.com>
2025-01-26 23:22:47 -0600

    global file search

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -4,13 +4,17 @@ import { getFiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
 import Card from "@/components/Card";
 import { getFileTypesParams } from "@/lib/utils";
+// @ts-expect-error: exists
+import { FileType, SearchParamProps } from "@/types/index";
 
-const Page = async ({ params }: SearchParamProps) => {
+const Page = async ({ searchParams, params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
 
   const types = getFileTypesParams(type) as FileType[];
 
-  const files = await getFiles({ types });
+  const files = await getFiles({ types, searchText, sort });
 
   return (
     <div className="page-container">
