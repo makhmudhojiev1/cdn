ce6da52b05b6318d6f6a9f2c3eb68d6042336318
nayanpai <nayanspai@gmail.com>
2025-01-26 18:49:05 -0600

    HTML syntax mistake

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -20,10 +20,8 @@ const Page = async ({ params }: SearchParamProps) => {
           </p>
 
           <div className="sort-container">
-            <p className="body-1 hidden text-light-200 sm:block">
-              Sort by:
-              <Sort />
-            </p>
+            <p className="body-1 hidden text-light-200 sm:block">Sort by:</p>
+            <Sort />
           </div>
         </div>
       </section>
